49675d8cd725ff79d9b41a174e4b940d46906495
ArjunaReddy <49161558+ArjunaReddy@users.noreply.github.com>
2019-11-21 15:59:21 +0530

    Update Users.java

--- RTOproject/src/Users.java
+++ RTOproject/src/Users.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.awt.event.ActionEvent;
 
 public class Users 
@@ -47,14 +51,34 @@ public class Users
 	private void initialize() 
 	{
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 548, 292);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(0, 0, 434, 176);
+		textArea.setBounds(0, 0, 522, 180);
 		frame.getContentPane().add(textArea);
-		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver"); 
+            Connection conn = null;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/rto","root", ""); 
+            System.out.print("Database is connected !");
+            Statement stmt = conn.createStatement();
+            String q1="select * from users;";
+            ResultSet rs = stmt.executeQuery(q1); 
+            textArea.setText("FName\tMName\tLName\tAadhar\tAddress\tPhone No\n");
+            while(rs.next())
+            {
+            	String pre=textArea.getText();
+            	String data1 = rs.getString("FName");String data2 = rs.getString("MName");String data3 = rs.getString("LName");
+            	String data4 = rs.getString("aadhar");String data5 = rs.getString("address");String data6 = rs.getString("phno");
+            	textArea.setText(pre+data1+"\t"+data2+"\t"+data3+"\t"+data4+"\t"+data5+"\t"+data6+"\n");
+            } 
+			conn.close();
+		}
+		catch(Exception e) {};
+
 		JButton btnAddUser = new JButton("Add User");
 		btnAddUser.addActionListener(new ActionListener()
 		{
@@ -64,9 +88,9 @@ public class Users
 				usr.frame.setVisible(true);
 			}
 		});
-		btnAddUser.setBounds(69, 207, 89, 23);
+		btnAddUser.setBounds(95, 207, 89, 23);
 		frame.getContentPane().add(btnAddUser);
-		
+
 		JButton btnDl = new JButton("DL");
 		btnDl.addActionListener(new ActionListener()
 		{
@@ -76,9 +100,9 @@ public class Users
 				usr.frame.setVisible(true);
 			}
 		});
-		btnDl.setBounds(170, 207, 89, 23);
+		btnDl.setBounds(228, 207, 89, 23);
 		frame.getContentPane().add(btnDl);
-		
+
 		JButton btnLl = new JButton("LL");
 		btnLl.addActionListener(new ActionListener()
 		{
@@ -88,9 +112,9 @@ public class Users
 				usr.frame.setVisible(true);
 			}
 		});
-		btnLl.setBounds(271, 207, 89, 23);
+		btnLl.setBounds(367, 207, 89, 23);
 		frame.getContentPane().add(btnLl);
-		
+
 		JButton btnPrev = new JButton("prev");
 		btnPrev.addActionListener(new ActionListener()
 		{
